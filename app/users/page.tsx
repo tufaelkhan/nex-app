a077fb5a121da954ebd9e66a918ee46ad64c07cb
tufaelkhan <tufaelkhan247@gmail.com>
2023-10-17 11:57:03 -0700

    data fetch

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -7,7 +7,8 @@ interface User{
 
 const UsersPage =  async () => {
   const res = await fetch('https://jsonplaceholder.typicode.com/users',{
-    next: { revalidate: 10}
+    cache: 'no-store'
+    // next: { revalidate: 10}
   })
   const users: User[] =  await res.json();
 
