1ca02c8b4e19c57649ae1f43fbbe22b4e47bbe22
tufaelkhan <tufaelkhan247@gmail.com>
2023-10-17 11:21:03 -0700

    add data

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -1,10 +1,10 @@
-'use client'
 import React from 'react'
+import AddToCart from './AddToCart'
 
 const ProductCard = () => {
   return (
     <div>
-        
+        <AddToCart/>
     </div>
   )
 }
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,8 +1,26 @@
 import React from 'react'
 
-const UsersPage = () => {
+interface User{
+  id: number;
+  name: string;
+}
+
+const UsersPage =  async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users',{
+    next: { revalidate: 10}
+  })
+  const users: User[] =  await res.json();
+
   return (
-    <div>UsersPage</div>
+    <>
+      <h1>Users</h1>
+      <p>{new Date().toLocaleTimeString()}</p>
+      <ul>
+        {
+          users.map(user => <li key={user.id}>{user.name}</li>)
+        }
+      </ul>
+    </>
   )
 }
 
